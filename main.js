396342cd396f6bd79f61077e07da8dd7420cbb68
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2023-03-19 23:05:32 +0530

    icon added

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
 import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
+import VectorLayer from "ol/layer/Vector.js";
 import OSM from 'ol/source/OSM';
-
+import { fromLonLat } from "ol/proj";
+import VectorSource from "ol/source/Vector.js";
+import Feature from "ol/Feature.js";
+import Point from "ol/geom/Point.js";
+import Style from "ol/style/Style";
+import Icon from "ol/style/Icon";
 const map = new Map({
-  target: 'map',
+  target: "map",
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
+      source: new OSM(),
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        features: [
+          new Feature({
+            geometry: new Point(fromLonLat([0, 0])),
+          }),
+        ],
+      }),
+      style: new Style({
+        image: new Icon({
+          src: "./assets/placeholder.png",
+          scale: 0.05,
+        }),
+      }),
+    }),
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+    center: fromLonLat([0, 0]),
+    zoom: 2,
+  }),
 });
