61371da24678b80cf04be141ba74f5d56be38872
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2023-03-22 11:47:44 +0530

    UI fix

--- main.js
+++ main.js
@@ -14,77 +14,86 @@ import { ref, onValue, get, child } from "firebase/database";
 import LayerGroup from "ol/layer/Group";
 const starCountRef = ref(db, "users");
 onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
-  console.log("DATA : ", data);
+  window.setInterval(() => {
+    const data = snapshot.val();
+    console.log("DATA : ", data);
 
-  var allLocation = [];
-  var colors = ["blue", "purple", "red", "yellow"];
-  var index = 0;
-  for (var location in data) {
- 
-    
-    allLocation.push(
-      new VectorLayer({
-        source: new VectorSource({
-          features: [
-            new Feature({
-              geometry: new Point(
-                fromLonLat([data[location].longitude, data[location].latitude])
-              ),
-            }),
-          ],
-        }),
-        style: new Style({
-          image: new Icon({
-            src: `./assets/placeholder_${colors[index]}.png`,
-            scale: 0.05,
+    var allLocation = [];
+    var colors = ["blue", "purple", "red", "yellow"];
+    var index = 0;
+    for (var location in data) {
+      allLocation.push(
+        new VectorLayer({
+          source: new VectorSource({
+            features: [
+              new Feature({
+                geometry: new Point(
+                  fromLonLat([
+                    data[location].longitude,
+                    data[location].latitude,
+                  ])
+                ),
+              }),
+            ],
           }),
-        }),
-      })
-    );
-    index = (index + 1) % 4;
-  }
+          style: new Style({
+            image: new Icon({
+              src: `./assets/placeholder_${colors[index]}.png`,
+              scale: 0.05,
+            }),
+          }),
+        })
+      );
+      index = (index + 1) % 4;
+    }
 
-  const map = new Map({
-    target: "map",
-    layers: [
-      new TileLayer({
-        source: new OSM(),
+    const map = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+        new LayerGroup({
+          title: "Year Comparison",
+          layers: allLocation,
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([data[location].longitude, data[location].latitude]),
+        zoom: 7,
       }),
-      new LayerGroup({
-        title: "Year Comparison",
-        layers: allLocation,
-      }),
-    ],
-    view: new View({
-      center: fromLonLat([data["akash"].longitude, data["akash"].latitude]),
-      zoom: 7,
-    }),
-  });
+    });
 
     for (var location in data) {
- 
-    
-    
-     var overlay = new Overlay({
-    position: fromLonLat([data[location].longitude, data[location].latitude]),
-    positioning: 'bottom-center',
-    element: document.createElement("div"),
-    stopEvent: false,
-  });
+      var overlay = new Overlay({
+        position: fromLonLat([
+          data[location].longitude,
+          data[location].latitude,
+        ]),
+        positioning: "bottom-center",
+        element: document.createElement("div"),
+        stopEvent: false,
+      });
 
-  overlay.getElement().innerHTML = `<div class="card">
+      overlay.getElement().innerHTML = `<div class="card">
  
-  <div class="card-body">
+  <div class="card-body" >
+   <div class="card-header">
+    Device Info
+  </div>
     <p><strong>Device Name:</strong>${data[location].Dname}</p>
+    <p><strong>IP Address:</strong>${data[location].ip}</p>
+    <p><strong>longitude:</strong>${data[location].longitude}</p>
+    <p><strong>latitude:</strong>${data[location].latitude}</p>
  
   </div>
-</div>`
+</div>`;
 
-  overlay.getElement().style.fontSize = "16px";
-  map.addOverlay(overlay);
-    index = (index + 1) % 4;
-  }
-
+      overlay.getElement().style.fontSize = "16px";
+      map.addOverlay(overlay);
+      index = (index + 1) % 4;
+      console.log(location)
+    }
+  }, 5000);
    
 });
