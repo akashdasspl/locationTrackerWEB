09063895333e6001a1d34b7d8f29159f94a77308
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2023-04-04 16:51:34 +0530

    UI and multiple marker update

--- main.js
+++ main.js
@@ -10,90 +10,160 @@ import Point from "ol/geom/Point.js";
 import Style from "ol/style/Style";
 import Icon from "ol/style/Icon";
 import { db } from "./config";
-import { ref, onValue, get, child } from "firebase/database";
+import { ref, onValue, query, orderByChild } from "firebase/database";
 import LayerGroup from "ol/layer/Group";
-const starCountRef = ref(db, "users");
-onValue(starCountRef, (snapshot) => {
-  window.setInterval(() => {
-    const data = snapshot.val();
-    console.log("DATA : ", data);
+import { MultiPoint } from "ol/geom";
 
+var data;
+const starCountRef = ref(db, "users/");
+var entries
+
+ 
+onValue(starCountRef, (snapshot) => {
+  
+  const data1 = snapshot.val();
+  console.log("DATA : ", data1);
+  data = data1;
+  
+
+
+ 
+  entries = Object.entries(data1);
+ entries.sort((a, b) => {
+   return a[1].time - b[1].time;
+ });
+ console.log(entries); 
+
+ 
+ var color = ["blue", "purple", "red", "yellow"];
+ var index = 0;
+
+let text = "<div class=cardd>";
+for (let i = 1; i < entries.length; i++) {
+
+ 
+ 
+
+   let p = entries[i][1].previousDATA.length
+   console.log(p)
+
+  text +=
+    ` <div class=cardbox style=background-color:${color[index]}  > ` +
+    "<p><strong>Device Name:</strong>" +
+    entries[i][1].Dname +
+    "</p>" +
+    "<p><strong>IP Address:</strong>" +
+    entries[i][1].ip +
+    "</p>" +
+    "<p><strong>longitude:</strong>" +
+    entries[i][1].longitude +
+    "</p>" +
+    "<p><strong>latitude:</strong>" +
+    entries[i][1].latitude +
+    "</p>" +
+    "<p><strong>previous latitude:</strong>" +
+    entries[i][1].previousDATA[p - 1].latitude +
+    "</p>" +
+    "<p><strong>previous longitude:</strong>" +
+    entries[i][1].previousDATA[p - 1].longitude +
+    "</p>" +
+    "<p><strong>time:</strong>" +
+    entries[i][1].Currenttime +
+    "</p>" +
+    "</div>";
+
+  
+  
+  
+  
+ index = (index + 1) % 4;
+}
+text += "</div>";
+
+document.getElementById("demo").innerHTML = text;
+ 
+
+});
+
+
+ 
+
+
+console.log("gfgfg", data);
+setTimeout(() => {
+  console.log("gfgfgset", data);
+  const fdsa = data;
+  console.log("fdsa", fdsa);
+  setTimeout(() => {
     var allLocation = [];
     var colors = ["blue", "purple", "red", "yellow"];
     var index = 0;
-    for (var location in data) {
-      allLocation.push(
-        new VectorLayer({
-          source: new VectorSource({
-            features: [
-              new Feature({
-                geometry: new Point(
-                  fromLonLat([
-                    data[location].longitude,
-                    data[location].latitude,
-                  ])
-                ),
-              }),
-            ],
-          }),
-          style: new Style({
-            image: new Icon({
-              src: `./assets/placeholder_${colors[index]}.png`,
-              scale: 0.05,
-            }),
-          }),
-        })
-      );
-      index = (index + 1) % 4;
-    }
 
-    const map = new Map({
-      target: "map",
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-        new LayerGroup({
+    const tileLayer = new TileLayer({
+      source: new OSM(),
+    });
+
+     
+var markerFeature;
+
+ for (let i = 1; i < entries.length; i++) {
+   var markerppoint = new Point(
+     fromLonLat([entries[i][1].longitude, entries[i][1].latitude])
+   );
+   markerFeature = new Feature({
+     geometry: markerppoint,
+   });
+   var vectorLayer = new VectorLayer({
+     source: new VectorSource({
+       features: [markerFeature],
+     }),
+     style: new Style({
+       image: new Icon({
+         src: `./assets/placeholder_${colors[index]}.png`,
+         scale: 0.05,
+       }),
+     }),
+   });
+
+   allLocation.push(vectorLayer);
+
+   index = (index + 1) % 4;
+ }
+const tileLayerGroup = new LayerGroup({
           title: "Year Comparison",
           layers: allLocation,
-        }),
-      ],
+        })
+    const map = new Map({
+      target: "map",
+      layers: [tileLayer, tileLayerGroup],
       view: new View({
-        center: fromLonLat([data[location].longitude, data[location].latitude]),
+        center: fromLonLat([
+          entries[entries.length - 1][1].longitude,
+          entries[entries.length - 1][1].latitude,
+        ]),
         zoom: 7,
       }),
     });
 
-    for (var location in data) {
-      var overlay = new Overlay({
-        position: fromLonLat([
-          data[location].longitude,
-          data[location].latitude,
-        ]),
-        positioning: "bottom-center",
-        element: document.createElement("div"),
-        stopEvent: false,
-      });
-
-      overlay.getElement().innerHTML = `<div class="card">
  
-  <div class="card-body" >
-   <div class="card-header">
-    Device Info
-  </div>
-    <p><strong>Device Name:</strong>${data[location].Dname}</p>
-    <p><strong>IP Address:</strong>${data[location].ip}</p>
-    <p><strong>longitude:</strong>${data[location].longitude}</p>
-    <p><strong>latitude:</strong>${data[location].latitude}</p>
+function agg(){
+     
+  
+for (let i = 1; i < entries.length; i++) {
  
-  </div>
-</div>`;
-
-      overlay.getElement().style.fontSize = "16px";
-      map.addOverlay(overlay);
-      index = (index + 1) % 4;
-      console.log(location)
-    }
-  }, 5000);
-   
-});
+  markerFeature.setGeometry(
+    new Point(fromLonLat([entries[i][1].longitude, entries[i][1].latitude]))
+  );
+ 
+} vectorLayer.changed();
+}
+    
+      
+           window.setInterval(() => {
+     
+     
+ agg()
+     }, 2000);
+  
+   }, 2000);
+}, 2000);
