60455e2be6e56fa4850cef3d607f9d7037b0d03a
akashdasspl <akashdasspl@gmail.com>
2023-06-17 00:21:21 +0530

    first commit

--- config.jsx
+++ config.jsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+//import {getAnalytics} from 'firebase/analytics';
 import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,17 +8,19 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIPS00f_56pbPvD63_MMYPKgdKUX442fw",
-  authDomain: "tracklocation-c12fe.firebaseapp.com",
-  databaseURL: "https://tracklocation-c12fe-default-rtdb.firebaseio.com",
-  projectId: "tracklocation-c12fe",
-  storageBucket: "tracklocation-c12fe.appspot.com",
-  messagingSenderId: "492761724996",
-  appId: "1:492761724996:web:3c3b62daafa75a1a3ea411",
-  measurementId: "G-955TBJWDXD",
+  apiKey: "AIzaSyD4-6cwEPuLs3ORvK6ekfhKey8hUDcmcgQ",
+  authDomain: "location-fb0c4.firebaseapp.com",
+  databaseURL: "https://location-fb0c4-default-rtdb.firebaseio.com",
+  projectId: "location-fb0c4",
+  storageBucket: "location-fb0c4.appspot.com",
+  messagingSenderId: "503951675041",
+  appId: "1:503951675041:web:f8516fb9b340a6f486aae0",
+  measurementId: "G-Y738B9RL47"
 };
 
 // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
 const app = initializeApp(firebaseConfig);
-
+//const analytics = getAnalytics(app);
 export const db = getDatabase(app);
